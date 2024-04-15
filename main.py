0ce9ee6598c718aa0ea052d10d1190a6f3628e20
janus103 <shinjin0103@seoultech.ac.kr>
2024-04-15 23:14:44 +0900

    customMLP

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 
 import dataset
 from dataset import MNIST
-from model import LeNet5
+from model import LeNet5, CustomMLP
 import torch
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
-from torchsummary import summary
+import os
 # import some packages you need here
 
 
-def train(model, trn_loader, device, criterion, optimizer,tst_loader,max_epoch):
+def train(model, trn_loader, device, criterion, optimizer,tst_loader,max_epoch,batch_size):
     """ Train function
 
     Args:
@@ -24,25 +24,36 @@ def train(model, trn_loader, device, criterion, optimizer,tst_loader,max_epoch):
         trn_loss: average loss value
         acc: accuracy
     """
+   
+    avg_loss=0
     for e in range (max_epoch):
         model.train()
+        total_correct = 0
         for i, (images,labels) in enumerate(trn_loader):
+            images=images.to("cuda")
+            labels=labels.to("cuda")
             optimizer.zero_grad()
             output = model(images)
+            pred = output.detach().max(1)[1]
+            total_correct += pred.eq(labels.view_as(pred)).sum()
             loss = criterion(output,labels)
-            loss_val = loss.float()
+            avg_loss += loss.sum()
             loss.backward()
-            optimizer.step
-            if(i%10==0):
-                print('Epoch: [{0}][{1}/{2}]\t'
-                'Loss {loss:.4f} \t'.format(
-                e, i, len(trn_loader), loss=loss_val))
-        test(model,tst_loader,device,criterion,e)
+            optimizer.step()
+        acc = 100 * float(total_correct) / torch.tensor(60000)
+        avg_loss = avg_loss / len(trn_loader)
+        print('Epoch: [{0}]\t'
+        'Train Accuracy {acc:.4f}\t'
+        'Train Loss {loss:.4f} \t'.format(
+        e, acc=acc,loss=avg_loss, ))
+
+
+        t_loss,acc = test(model,tst_loader,device,criterion,e,batch_size)
     # write your codes here
 
-    return trn_loss, acc
+    return loss, acc
 
-def test(model, tst_loader, device, criterion,epoch=None):
+def test(model, tst_loader, device, criterion,epoch=None,batch_size=None):
     """ Test function
 
     Args:
@@ -59,26 +70,30 @@ def test(model, tst_loader, device, criterion,epoch=None):
     total_correct = 0
     avg_loss = 0.0
     for i, (images, labels) in enumerate(tst_loader):
+        images=images.to("cuda")
+        labels=labels.to("cuda")
+
         output = model(images)
         avg_loss += criterion(output, labels).sum()
         pred = output.detach().max(1)[1]
         total_correct += pred.eq(labels.view_as(pred)).sum()
-    avg_loss /= len(data_test)
-    acc = float(total_correct) / len(data_test)
+    avg_loss /= len(tst_loader)
+  
+    acc = 100 * float(total_correct) / torch.tensor(10000)
 
     if(epoch is not None):
         print('Epoch: [{0}]\t'
-        'Test Accuracy: {acc:.4f}'
-        'Loss {loss.val:.4f} \t'.format(
+        'Test Accuracy {acc:.4f}\t'
+        'Test Loss {loss:.4f} \t'.format(
         epoch, acc=acc,loss=avg_loss, ))
     else:
-        print('Test Accuracy: {acc:.4f}'
-        'Loss {loss.val:.4f} \t'.format(
-        acc=acc, loss=avg_loss, ))
+         print('Test Accuracy {acc:.4f} %\t'
+        'Test Loss {loss:.4f} \t'.format(
+        acc=acc,loss=avg_loss, ))
 
     # write your codes here
 
-    return tst_loss, acc
+    return avg_loss, acc
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     """
@@ -95,13 +110,16 @@ def main():
         4) optimizer: SGD with initial learning rate 0.01 and momentum 0.9
         5) cost function: use torch.nn.CrossEntropyLoss
     """
+    #device setting
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     train_dir = "./data/train"
     test_dir = "./data/test"
     #dataset param
     batch_size = 512
-    shuffle = False
+    shuffle = True
     num_workers = 16
-
+    
     train_set = MNIST(train_dir)
     test_set = MNIST(test_dir)
 
@@ -110,24 +128,44 @@ def main():
 
     #import model
     model = LeNet5()
-    model.train()
     model.cuda()
+    
+    #check params
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"LeNet5 model parameter num : {total_params}")
+    
+
     # train param
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.1,momentum=0.9)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epoch = 16
-    ## train start
-    print("train start")
-    train(model,train_loader,device,criterion,optimizer,test_loader,epoch)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01,momentum=0.9)
+   
+    epoch = 30
+    # LeNet train start
+
+    print("LeNet train start")
+    model.train()
+    train(model,train_loader,device,criterion,optimizer,test_loader,epoch,batch_size)
 
     ##test start
-    test(model,test_loader,device,criterion)
-    #save weight
-    save_path = "weight"
-    save_checkpoint({
-                'state_dict': model.state_dict(),
-            }, True, filename=os.path.join(save_path,'LeNet5.pt'))
-    print("train done")
+    acc ,loss = test(model,test_loader,device,criterion,batch_size=batch_size)
+    print("LeNet train done")
+
+
+
+    model_2 = CustomMLP()
+    model_2.cuda()
+    model_2.train()
+    criterion_2 = nn.CrossEntropyLoss()
+    optimizer_2 = torch.optim.SGD(model_2.parameters(), lr=0.01,momentum=0.9)
+   
+    #check params
+    total_params = sum(p.numel() for p in model_2.parameters())
+
+    print(f"LeNet5 model parameter num : {total_params}")
+    print("Custom train start")
+    train(model_2,train_loader,device,criterion_2,optimizer_2,test_loader,epoch,batch_size)
+    acc ,loss = test(model_2,test_loader,device,criterion_2,batch_size=batch_size)
+    print("Custom train done")
+
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -14,16 +14,16 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
         self.activation = nn.SiLU()
 
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5,stride=1,padding=2)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5,stride=1,padding=2) 
         self.MaxPool_1 = nn.MaxPool2d(kernel_size=(2,2),stride=2)
 
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5) 
         self.MaxPool_2 = nn.MaxPool2d(kernel_size=(2,2),stride=2)
 
-        self.conv3 = nn.Conv2d(6, 16, kernel_size=5)
+        self.conv3 = nn.Conv2d(6, 16, kernel_size=5) 
         self.MaxPool_3 = nn.MaxPool2d(kernel_size=(2,2),stride=2)
 
-        self.conv4 = nn.Conv2d(16, 120, kernel_size=5)
+        self.conv4 = nn.Conv2d(16, 120, kernel_size=5) 
         self.FC_1 = nn.Linear(120,84)
         self.FC_2 = nn.Linear(84,10)
         self.softmax = nn.LogSoftmax(dim=-1)
@@ -41,19 +41,39 @@ class LeNet5(nn.Module):
         return x
 
 
-# class CustomMLP(nn.Module):
-#     """ Your custom MLP model
+class CustomMLP(nn.Module):
+    """ Your custom MLP model
 
-#         - Note that the number of model parameters should be about the same
-#           with LeNet-5
-#     """
+        - Note that the number of model parameters should be about the same
+          with LeNet-5
+    """
 
-#     def __init__(self):
+    def __init__(self):
+        super(CustomMLP, self).__init__()
+        # conv params = 52950 
+        input_size = 28*28
+        hidden_size = 58
 
-#         # write your codes here
+        # mlp parameter size = input_size[28*28 = 784] * hidden_size +  hidden_size * 120 < 52,950 (LeNet Conv Params)
+        # -> max hidden_size = 58 (58*784 + 58*120 = 52,432)
 
-#     def forward(self, img):
+        self.mlp = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.SiLU(),
+            nn.Linear(hidden_size, 120),
+            nn.BatchNorm1d(120)
+        )
 
-#         # write your codes here
-
-#         return output
+        self.FC_1 = nn.Linear(120,84)
+        nn.BatchNorm1d(84),
+        self.FC_2 = nn.Linear(84,10)  # LeNet-5의 출력 크기는 10
+        self.softmax = nn.LogSoftmax(dim=-1)
+        self.activation = nn.SiLU()
+    def forward(self, x):
+        x = x.view(-1, 28*28)  # 입력 이미지를 1차원으로 평탄화
+        x = self.mlp(x)
+        x = self.activation(self.FC_1(x))
+        x = self.activation(self.FC_2(x))
+        x = self.softmax(x)
+        return x
